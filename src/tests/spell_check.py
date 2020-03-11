56e12d11d7a0761d334c5afaf903168d28755aed
mike haberman <haberman@illinois.edu>
2020-03-10 19:23:11 -0700

    reorg

---
+++ src/tests/spell_check.py
@@ -0,0 +1,42 @@
+
+
+import sys
+sys.path.append('..')
+
+from utils import Parser
+from utils import Tools
+
+n_id ='1ymVhzIS-TCKhOx28jWEQ3E2IxWscGwwA'
+sys.path.append('..')
+#txt = Tools.install_gd_file(n_id, 'tmp.txt', force=True)
+txt = Tools.install_gd_file(n_id, 'tmp.txt', force=True, persist=True)
+
+parser = Parser.NBParser()
+txt = parser.parse(txt, markdown=True)
+
+open('what.txt', 'w').write(txt)
+
+def clean_markdown(txt):
+    clean = []
+    for l in txt.split('\n'):
+        line = l.strip()
+        if len(line) == 0:
+            continue
+        if line.startswith('$') and line.endswith('$'):
+            continue
+        tokens = []
+        for token in line.split():
+            token = token.replace('$', ' ')
+            token = token.replace('[', ' ')
+            token = token.replace(']', ' ')
+            token = token.replace('\\', ' ')
+            token = token.replace('#', '')
+            token = token.replace('*', '')
+            tokens.append(token)
+        clean.append(' '.join(tokens) + '\n')
+    return '\n'.join(clean)
+
+print(clean_markdown(txt))
+
+#  python spell_check.py  > out2.txt
+#   604  aspell check out2.txt
